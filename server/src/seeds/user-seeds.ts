e34a12eb9f0f5ef3f7feed652093b1f09dfb63fa
Edward O <edwardoreilly0906@gmail.com>
2025-03-25 21:41:55 -0500

    Updated package.json/ user-seeds file

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -3,6 +3,6 @@ import { User } from '../models/index.js';
 export const seedUsers = async () => {
   await User.bulkCreate([
     { username: 'Admin123', email: 'admin@gmail.com', password: 'password' },
-    
+    { username: 'eoreilly0906', email: 'eoreilly0906@gmail.com', password: 'password' },
   ], { individualHooks: true });
 };
